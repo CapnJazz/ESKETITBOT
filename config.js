18c71c67bc42030f49711bd65b01221a8e53a3b8
John M <32595184+CapnJazz@users.noreply.github.com>
2017-11-02 17:17:20 +1100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = { //module object with all the twitter API keys
+	consumer_key: 				'EkrCtWmgV0bILKL7XjtUT7t6W'
+	consumer_secret: 			'BzaA96Hvp38znf8Ioam5ZM4KjSPm3RCzw62fizs0KPP3DINyqY'
+	access_token: 				'925945704379334656-4I7kCPoF9pbClzEZwhva2oCx29BhN39'
+	access_token_secret:		'mGFljXBJF1riJeTpOUJXfGfGzLndj8O6OF9OblVa14dVl'
+}
